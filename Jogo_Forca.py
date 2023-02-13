b5be351d051a9e53fd7469ba117df07182bc0bb1
JP3015 <RM87894@fiap.com.br>
2023-02-13 18:50:05 -0300

    exibição das letras acertadas e a remoção de coisas desnecessárias

--- Jogo_Forca.py
+++ Jogo_Forca.py
@@ -52,7 +52,7 @@ def erro(erros):
    
 
 def checar(erros):
-    if acertos == len(mylist):
+    if len(letra_palavra) == 0:
         print("\nParabéns, você ganhou!")
         print("       ___________      ")
         print("      '._==_==_=_.'     ")
@@ -64,64 +64,73 @@ def checar(erros):
         print("           ) (          ")
         print("         _.' '._        ")
         print("        '-------'       ")
-
         
-
+        
     elif erros == 7:
-        print("\nPuxa, você foi enforcado!")
-        print("\nA palavra era {}".format(palavra))
-        print("    _______________         ")
-        print("   /               \       ")
-        print("  /                 \      ")
-        print("//                   \/\  ")
-        print("\|   XXXX     XXXX   | /   ")
-        print(" |   XXXX     XXXX   |/     ")
-        print(" |   XXX       XXX   |      ")
-        print(" |                   |      ")
-        print(" \__      XXX      __/     ")
-        print("   |\     XXX     /|       ")
-        print("   | |           | |        ")
-        print("   | I I I I I I I |        ")
-        print("   |  I I I I I I  |        ")
-        print("   \_             _/       ")
-        print("     \_         _/         ")
-        print("       \_______/           ")
+        string = "".join(str(x) for x in palavra)
+        print("Você foi enforcado! A palavra era " + string)
+
+
 
 opcao = 0
 
-if opcao != 3:
+while opcao != 2:
     print("*********************************")
     print("***Bem vindo ao jogo da Forca!***")
     print("*********************************")
 
     erros = 0
-    acertos = 0
     letras = []
-    palavra = list(random.choice(open(r"C:\Users\User\Desktop\Jogo_Forca\palavras.txt").readline().split()))
-    mylist = list(dict.fromkeys(palavra))
+    palavra = random.choice(open(r"C:\Users\User\Desktop\Jogo_Forca\palavras.txt").readline().split())
+    letra_palavra = set(palavra)
+    letras_acertadas = ["_"] * len(palavra)
     print(palavra)
+    
+    
+    while erros != 7 and len(letra_palavra) != 0:
+       
+        print("\nPalavra:", end=" ")
+        for tentativa in letras_acertadas:
+                print(tentativa, end=" ")
 
-    while erros != 7 and acertos != len(mylist):
-        
-        tentativa = input("\nEscolha uma letra: ")
+        print("\n")
+
+        tentativa = input("Escolha uma letra: ")
 
         while len(tentativa) > 1 or tentativa in letras:    
-            tentativa = input("\nVocê digitou mais de uma letra ou digitou uma letra que já foi escolhida, escolha uma outra letra: ")
-
+            tentativa = input("Você digitou mais de uma letra ou digitou uma letra que já foi escolhida, escolha uma outra letra: ")
+        
 
         letras.append(tentativa)
 
         if (tentativa in palavra):
-            acertos += 1
             print("Acertou")
-
+            letra_palavra.remove(tentativa)
+            for i in range(len(palavra)):
+                    if palavra[i] == tentativa:
+                        letras_acertadas[i] = tentativa
+            
         else:
             erros += 1
             print("Errou!!")
             erro(erros)
         
-       
 
         checar(erros)
 
-        
+
+    while True:
+        print("\nDigite 1 caso queira rejogar.\nDigite 2 caso queira sair.")
+        opcao = int(input(">>> "))
+
+        if opcao == 1:
+            print("\nReiniciando....")
+            break
+        elif opcao == 2:
+            print("\nSaindo.......")
+            break
+        else:
+            print("\nOpção inválida, por favor digite novamente.")
+
+    
+   
